5500a90341a9cd7845860ae7064754b7de5cca8e
Shubham Chouhan <shubamc04@gamil.com>
2026-09-02 11:14:39 +0530

    if else  statement

--- main.py
+++ main.py
@@ -1,4 +1,63 @@
 """FORMATING sTRING"""
-name ="shubham"
-age="22"
-print(f"my name is {name} and my age is {age}")
+
+#name ="shubham"
+
+#age="22"
+
+#print(f"my name is {name} and my age is {age}")
+
+#name = input("enter your name")
+
+3#print(name)
+
+""" Arithmatic opratore"""
+#a=12
+
+#b=34
+#print(a+b)
+
+#print(a-b)
+
+#print(a*b)
+
+#print(a//b)
+
+#print(a/b)
+
+#print(a**b)
+
+"""IF ELSE STATEMENT
+money =int(input("please provide me the moeny :-"))
+
+if money == 10:
+    print("you can buy a coffee")
+
+elif money ==20:
+    print("you can buy a coffee and a samosa")
+else:
+    print("you can buy a coffee and a samosa and a cake")"""
+
+
+"""a=10
+b=30
+
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")"""
+
+"""gender=str(input("enter your gender :-"))
+if gender=="male":
+    print("Good  morning sir")
+elif gender=="female":
+    print("Good  morning ma'am")
+else:
+    print("Good  morning everyone")"""
+
+"""EVEN AND ODD NUMBER"""
+
+"""a=int(input("enter your number :-"))
+if a%2==0:
+    print("this is even number")
+else:
+    print("this is odd number")"""
